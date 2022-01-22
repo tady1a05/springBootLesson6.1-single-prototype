edcb6e4edbbcc734421dac4ddadbeeea6ed45da5
toby1a05 <32935616+toby1a05@users.noreply.github.com>
2022-01-22 06:47:44 -0800

    first commit

---
+++ src/com/test/springboot/swimCoach.java
@@ -0,0 +1,38 @@
+package com.test.springboot;
+
+public class swimCoach implements Coach{
+	FortunateServices fs;
+	String name;
+	String email;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setFs(FortunateServices fs) {
+		this.fs = fs;
+	}
+
+	@Override
+	public String doWorkout() {
+		// TODO Auto-generated method stub
+		return "Swim 75m";
+	}
+	
+	@Override
+	public String doFortunate() {
+		return fs.doFortunate();
+	}
+}
